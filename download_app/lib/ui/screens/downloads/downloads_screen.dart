b7b0880526a3d77c26688cb71777d82d66f01ba8
rscbpch <bopich568317@gmail.com>
2026-02-22 16:13:42 +0700

    done task-002 : downloader downloads

--- download_app/lib/ui/screens/downloads/downloads_screen.dart
+++ download_app/lib/ui/screens/downloads/downloads_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../../providers/theme_color_provider.dart';
 import '../../theme/theme.dart';
 import 'widgets/download_controler.dart';
-
+import 'widgets/download_tile.dart';
 
 class DownloadsScreen extends StatelessWidget {
   final ThemeColorProvider themeProvider;
@@ -10,7 +10,7 @@ class DownloadsScreen extends StatelessWidget {
   // Create the list of fake ressources
   final List<Ressource> ressources = [
     Ressource(name: "image1.png", size: 120),
-    Ressource(name: "image1.png", size: 500),
+    Ressource(name: "image2.png", size: 500),
     Ressource(name: "image3.png", size: 12000),
   ];
 
@@ -29,23 +29,21 @@ class DownloadsScreen extends StatelessWidget {
       listenable: themeProvider,
       builder: (context, _) {
         return Container(
+          width: double.infinity,
+          height: double.infinity,
           color: themeProvider.selected.backgroundColor,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               SizedBox(height: 16),
-              Text(
-                "Downloads",
-                style: AppTextStyles.heading.copyWith(
-                  color: themeProvider.selected.color,
-                ),
-              ),
+              Text("Downloads", style: AppTextStyles.heading.copyWith(color: themeProvider.selected.color)),
               SizedBox(height: 50),
               // TODO - Add the Download tiles
+              ...controllers.map((controller) => DownloadTile(controller: controller, themeProvider: themeProvider)),
             ],
           ),
         );
-      }
+      },
     );
   }
 }
--- download_app/lib/ui/screens/downloads/widgets/download_tile.dart
+++ download_app/lib/ui/screens/downloads/widgets/download_tile.dart
@@ -1,18 +1,76 @@
 import 'package:flutter/material.dart';
- 
+
+import '../../../theme/theme.dart';
+import '../../../providers/theme_color_provider.dart';
 import 'download_controler.dart';
 
 class DownloadTile extends StatelessWidget {
-  const DownloadTile({super.key, required this.controller});
+  const DownloadTile({super.key, required this.controller, required this.themeProvider});
 
   final DownloadController controller;
- 
- // TODO
+  final ThemeColorProvider themeProvider;
+
+  // TODO
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
-     
-    // TODO
+    return ListenableBuilder(
+      listenable: controller,
+      builder: (context, _) {
+        final ressource = controller.ressource;
+        final progress = controller.progress;
+        final status = controller.status;
+        final downloadedMB = (progress * ressource.size).toStringAsFixed(1);
+        final percentage = (progress * 100).toStringAsFixed(1);
+
+        String subtitle;
+        if (status == DownloadStatus.downloaded) {
+          subtitle = '100% completed -  $downloadedMB of ${ressource.size} MB';
+        } else if (status == DownloadStatus.downloading) {
+          subtitle = '$percentage % completed - $downloadedMB of ${ressource.size} MB';
+        } else {
+          subtitle = '';
+        }
+
+        return Container(
+          margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: ListTile(
+            contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+            title: Text(
+              ressource.name,
+              style: AppTextStyles.body.copyWith(color: AppColors.text)
+            ),
+            subtitle: Text(
+              subtitle,
+              style: AppTextStyles.label.copyWith(color: AppColors.textLight)
+            ),
+            trailing: status == DownloadStatus.notDownloaded
+                ? IconButton(
+                    icon: Icon(
+                      Icons.download, 
+                      color: AppColors.iconNormal,
+                      size: 28,
+                    ),
+                    onPressed: () => controller.startDownload(),
+                  )
+                : status == DownloadStatus.downloading
+                ? Icon(
+                    Icons.downloading,
+                    color: AppColors.iconNormal,
+                    size: 28,
+                  )
+                : Icon(
+                    Icons.folder,
+                    color: AppColors.iconNormal,
+                    size: 28,
+                  ),
+          ),
+        );
+      },
+    );
   }
 }
